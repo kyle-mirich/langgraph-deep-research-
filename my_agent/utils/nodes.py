a48597e92a7993e1fc31876fef7a6654efd60061
Kyle <94569133+kyle-mirich@users.noreply.github.com>
2025-07-12 20:35:50 -0700

    init commit

--- my_agent/utils/nodes.py
+++ my_agent/utils/nodes.py
@@ -3,16 +3,12 @@ from langchain_anthropic import ChatAnthropic
 from langchain_openai import ChatOpenAI
 from my_agent.utils.tools import tools
 from langgraph.prebuilt import ToolNode
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 
 @lru_cache(maxsize=4)
-def _get_model(model_name: str):
-    if model_name == "openai":
-        model = ChatOpenAI(temperature=0, model_name="gpt-4o")
-    elif model_name == "anthropic":
-        model =  ChatAnthropic(temperature=0, model_name="claude-3-sonnet-20240229")
-    else:
-        raise ValueError(f"Unsupported model type: {model_name}")
+def _get_model(model_name):
+    model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.0, max_output_tokens=None,timeout=None,max_retries=3,google_api_key="AIzaSyAB0GcW2FFoCKiD0z4Mmk6P-fm0lrNfiRI")
 
     model = model.bind_tools(tools)
     return model
